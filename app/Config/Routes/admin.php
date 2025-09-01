8d544a63c35f097e2d6f9a1803b4b0ff8a3ed0f9
poypo <poypoye@orange.fr>
2025-09-01 09:14:24 +0200

    feat: user terminé

--- app/Config/Routes/admin.php
+++ app/Config/Routes/admin.php
@@ -2,4 +2,20 @@
 $routes->group('admin', ['namespace' => 'App\Controllers\Admin', 'filter' => 'auth:administrateur'], function ($routes) {
     //Routes vers le tableau de bord
     $routes->get('dashboard', 'Admin::dashboard');
+
+    $routes->group('user', function ($routes) {
+        $routes->get('/', 'User::index');
+        $routes->get('(:num)', 'User::edit/$1');
+        $routes->get('new', 'User::create');
+        $routes->post('update', 'User::update');
+        $routes->post('insert', 'User::insert');
+        $routes->post('switch-active','User::switchActive');
+    });
+
+    $routes->group('user-permission', function ($routes) {
+       $routes->get('/', 'UserPermission::index');
+       $routes->post('update', 'UserPermission::update');
+       $routes->post('insert', 'UserPermission::insert');
+       $routes->post('delete', 'UserPermission::delete');
+    });
 });
